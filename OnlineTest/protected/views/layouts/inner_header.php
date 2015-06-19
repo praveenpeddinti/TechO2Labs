6ce923976e47d524b8f971620ff8988b884edf92
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@7eef34f4-4721-40fc-8f62-272adaec8920>
2015-06-19 05:35:13 +0000

    Online test
    git-svn-id: https://subversion.assembla.com/svn/techO2TTI.onlinetest/trunk@34 7eef34f4-4721-40fc-8f62-272adaec8920

--- OnlineTest/protected/views/layouts/inner_header.php
+++ OnlineTest/protected/views/layouts/inner_header.php
@@ -1,39 +1,41 @@
- <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-         <a class="navbar-brand" href="#"><img src="images/inner_techo2logo.png" ></a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="images/inner_techo2logo.png" ></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-          <div class="headernavigation">
-        <?php if(Yii::app()->params['Project']=='SkiptaNeo'){ include 'leftmenu.php'; }?>
-          
-          
-          <div style="float:right" class="profilearea position_R"><ul class="nav navbar-nav profileul">
+            <div class="headernavigation">
+                <?php if (Yii::app()->params['Project'] == 'SkiptaNeo') {
+                    include 'leftmenu.php';
+                } ?>
 
-            <li class=" profilelist dropdown">
-            <div class="generalprofileicon skiptaiconwidth60x60" >
-<a aria-expanded="false" role="button" data-toggle="dropdown" data-placement="right" class="dropdown-toggle skiptaiconinner " href="#"><img src="images/sreeni.png"> <span class="caret"></span></a> </a>
-<ul role="menu" class="dropdown-menu">
-                <li> <a href="/user/logout" id="logoutId" onclick="logout()"><i class="logOutIcon"><img src="/images/system/spacer.png" ></i><?php echo Yii::t('translation','Logout'); ?></a></li>
-                </ul>
 
-</div>
-            
-              
-              
-            </li>
-          </ul></div>
-          </div>
+                <div style="float:right" class="profilearea position_R"><ul class="nav navbar-nav profileul">
+
+                        <li class=" profilelist dropdown">
+                            <div class="generalprofileicon skiptaiconwidth60x60" >
+                                <a aria-expanded="false" role="button" data-toggle="dropdown" data-placement="right" class="dropdown-toggle skiptaiconinner " href="#"><img src="images/sreeni.png"> <span class="caret"></span></a> </a>
+                                <ul role="menu" class="dropdown-menu">
+                                    <li> <a href="/user/logout" id="logoutId" onclick="logout()"><i class="logOutIcon"><img src="/images/system/spacer.png" ></i><?php echo Yii::t('translation', 'Logout'); ?></a></li>
+                                </ul>
+
+                            </div>
+
+
+
+                        </li>
+                    </ul></div>
+            </div>
         </div><!--/.navbar-collapse -->
-      </div>
-      
-    </nav>
+    </div>
+
+</nav>
 
 
 
@@ -51,84 +53,7 @@
 
 
 
-<div id="sidebar"></div>
 
-<header id="header">
-     <!-- Modal -->
-            <div class="modal fade" id="newModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog ">
-                    <div class="modal-content ">
-                        <div class="modal-header" id="newModal_header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h4 class="modal-title" id="newModalLabel">Modal title</h4>
-                        </div>
-                        <div class="modal-body" id="newModal_body">
-
-                        </div>
-                        <div class="modal-footer" id="newModal_footer">
-                            <button type="button" class="btn btn-small" id="newModal_btn_primary"><?php echo Yii::t('translation','Save_changes'); ?></button>
-                            <button type="button" class="btn btn-small" data-dismiss="modal" id="newModal_btn_close"><?php echo Yii::t('translation','Close'); ?></button>
-                        </div>
-                    </div><!-- /.modal-content -->
-                </div><!-- /.modal-dialog -->
-            </div><!-- /.modal -->
-	<div class="container">
-    	<div class="row-fluid">
-        	<div class="span12">
-            	<div class="span2 iphonelogo">
-                    <a href="/"><img src="<?php echo Yii::app()->params['Logo']; ?>" alt="logo" class="logo"></a>                </div>
-              <div class="col-xs-12  col-md-9 col-sm-10" positionrelative" id='headerSection'>
-                  
-                <div class="pull-right mobileclear">
-                   
-                <ul class="nav headermenuarea ">
-                    
-                
-<!--                <li> <input type="button" value="loadJoyRide" onclick="getNewUserJoyrideDetails();"></li>-->
-                
-<!--                <li class="gameiconlist">
-                <img src="/images/system/removedimg.jpg"> 
-                  <a  href="#" class="gameicon"  data-original-title="Game" rel="tooltip" data-placement="bottom" lass="tooltiplink">&nbsp;</a>
-                </li>-->
-                <?php if(Yii::app()->params['Chat'] == "ON"){?>
-                 <li class="normal">
-                     <div class="not_count " id="chatOffCount" style="display: none"></div>
-                 <a  onclick="divrenderget('chatDiv','/chat/index')" id="chatIcon" class="chaticon"  data-original-title="<?php echo Yii::t('translation','Chat'); ?>" rel="tooltip" data-placement="bottom" lass="tooltiplink">&nbsp;</a>
-                 
-                </li>
-                <?php } ?>
-                 
-                
-                 <li class="normal" id='profileDropDown'>
-                     <div  data-toggle="dropdown" id="drop3" class="headerprofileicon skiptaiconwidth43x43 skiptaiconmargintop6">
-                  <a  href="#" rel="tooltip" data-placement="bottom"  class="skiptaiconinner submenu" data-original-title="<?php echo Yii::app()->session['TinyUserCollectionObj']['DisplayName']?>" >  <img id="profileImage_header" src=<?php echo Yii::app()->session['TinyUserCollectionObj']['profile250x250']?> > </a>
-                     </div>
-                  <div class="dropdown-menu profilewidth" >
-
-               <form style="margin: 0px" accept-charset="UTF-8" action="/" method="post">
-                <div id="UserDisplayName" class="headerpoptitle"><?php echo Yii::app()->session['TinyUserCollectionObj']['DisplayName']?></div>
-                
-                <ul>
-                
-               
-                <li> <a href="/user/logout" id="logoutId" onclick="logout()"><i class="logOutIcon"><img src="/images/system/spacer.png" ></i><?php echo Yii::t('translation','Logout'); ?></a></li>
-              
-                </ul>
-                
-               </form>
-             </div>
-                 	 
-                </li>
-                </ul>
-               
-                </div>
-            </div>
-            </div>
-       
-        </div>
-           
-     </div>
-</header>
 <div id="norecordsFound" style="display: none" ></div>
 
 
