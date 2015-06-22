a71d0a4bf71cc9d9acb3e86f1a9e9e3a8b028d44
sateesh.mandru <sateesh.mandru@7eef34f4-4721-40fc-8f62-272adaec8920>
2015-06-22 13:11:41 +0000

    styles for Inner Flow
    git-svn-id: https://subversion.assembla.com/svn/techO2TTI.onlinetest/trunk@76 7eef34f4-4721-40fc-8f62-272adaec8920

--- OnlineTest/protected/views/layouts/header_prelogin.php
+++ OnlineTest/protected/views/layouts/header_prelogin.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
-	<!-- blueprint CSS framework -->
+        <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/favicon.png" type="image/x-icon" />
+	
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
 	<!--[if lt IE 8]>
