0e703336359d7870fc7b4fa81f94f29ac363a0b8
praveen.peddinti@techo2.com <praveen.peddinti@techo2.com@7eef34f4-4721-40fc-8f62-272adaec8920>
2015-07-06 08:30:58 +0000

    Test paper Reports commits
    git-svn-id: https://subversion.assembla.com/svn/techO2TTI.onlinetest/trunk@104 7eef34f4-4721-40fc-8f62-272adaec8920

--- OnlineTest/protected/views/reports/index.php
+++ OnlineTest/protected/views/reports/index.php
@@ -6,7 +6,24 @@
         <div class="span12"><h2 class="pagetitle" id="pagetitle">Score Board</h2></div>
       
     </div>
-  
+  <div id="inviteuser_div">
+        <div style="position: relative">
+            <div  class="block">
+                
+                <table cellspacing="0" cellpadding="0" width="100%" border="0" class="dtb_header">
+                    <thead><tr><th class="data_t_hide">Name</th><th class="data_t_hide">Date</th><th class="data_t_hide">Total Questions</th></tr></thead>
+                    <tbody>
+                        <tr id="noRecordsTR" style="display: none">
+                            <td colspan="8">
+                                <span class="text-error"> <b>No records found</b></span>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>        
+        </div>
+    </div>
 </div>    
     
     
