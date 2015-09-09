a4641b5f1c3caf4097b255f96477db68d903feb6
vamsi.nallana <vamsi.nallana@a14b6947-d413-41b1-9c5c-8c880e728f1c>
2015-09-09 12:58:46 +0000

    I have been modifying controller modifying controller as I included init method
    git-svn-id: https://subversion.assembla.com/svn/techO2TTI.YiiSkelton/trunk@11 a14b6947-d413-41b1-9c5c-8c880e728f1c

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,4 +20,12 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+        
+        public $myservices;
+        
+        public function init()
+        {
+            $this->myservices =  new InstantEmpStorage();
+            
+        }
 }
