6068ca3ff1aaf96d82d6bb4093e456e3453ef31f
vinodmeda <vinodmeda@a14b6947-d413-41b1-9c5c-8c880e728f1c>
2015-09-09 11:32:28 +0000

    1). Allprofiles link added.
    git-svn-id: https://subversion.assembla.com/svn/techO2TTI.YiiSkelton/trunk@9 a14b6947-d413-41b1-9c5c-8c880e728f1c

--- protected/views/layouts/header.php
+++ protected/views/layouts/header.php
@@ -61,6 +61,7 @@ if (isset($session) && count($session) > 0) {
                                         <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
                                            <li><a href="<?php  echo $this->createUrl('Techo2Employee/EmployeeProfile',array('employee_id'=>$employee_id)); ?>">View profile</a></li>
                                            <li><a href="<?php  echo $this->createUrl('Techo2Employee/EditEmployeeProfile',array('employee_id'=>$employee_id)); ?>">Edit profile</a></li>
+                                           <li><a href="<?php  echo $this->createUrl('Techo2Employee/AllProfiles'); ?>">All Profiles</a></li>
                                             <li><a href="<?php  echo Yii::app()->request->baseUrl.'/Techo2Employee/LoggedOut'; ?>">Log out</a></li>
                                         </ul>
                                     </div>
